d3bd3a6e09cfb36e85843a5ccf6b78808b1de89b
chepupel123 <dauletkeldinurali@gmail.com>
2026-09-09 00:36:14 +0500

    Delete CLauncher directory

--- CLauncher/src/version_scanner.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-
-class VersionScanner {
-public:
-    static std::string get_versions_path();
-    static std::vector<std::string> scan_versions();
-};
